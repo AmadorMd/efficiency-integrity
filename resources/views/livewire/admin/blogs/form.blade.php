0c4d313ba23532622b9b56678b089f74904b34f7
Luis Amador <amadormd912@gmail.com>
2023-03-17 00:13:15 -0600

    add: loading state and disabled button on loading forms

--- resources/views/livewire/admin/blogs/form.blade.php
+++ resources/views/livewire/admin/blogs/form.blade.php
@@ -28,8 +28,21 @@
             <span class="text-sm font-bold text-red-500">{{ $message }}</span>
         @enderror
         <div class="mt-5">
-            <button class="bg-indigo-400 text-xs px-3 py-2 text-white tracking-wide rounded-full font-bold block w-full">
-                Guardar Información
+            <span class="bg-gray-500"></span>
+            <button class="bg-indigo-400 text-xs px-3 py-2 text-white tracking-wide rounded-full font-bold block w-full" wire:loading.class="bg-gray-500"  wire:loading.class.remove="bg-indigo-400 " wire:loading.attr="disabled">
+                <span wire:loading.remove>
+                    Guardar Información
+                </span>
+                <div wire:loading>
+                    <div class="inline-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 animate-spin">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                          </svg>
+                          <span class="ml-2">Cargando...</span>      
+                    </div>
+                                 
+                    
+                </div>
             </button>
         </div>
     </form>
