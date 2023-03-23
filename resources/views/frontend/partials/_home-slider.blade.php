7113aa7c3bbe735da19e5ee6ced7b209a2a35b55
Luis Amador <amadormd912@gmail.com>
2023-03-22 18:08:29 -0600

    fix:banner typo and changes images

--- resources/views/frontend/partials/_home-slider.blade.php
+++ resources/views/frontend/partials/_home-slider.blade.php
@@ -36,7 +36,9 @@
             <div id="slide1" class="relative">
                 <img class="relative z-0" src="{{ asset('images/main-banner/main_banner-3.jpg') }}" alt="">
                 <div class="absolute z-10 w-2/3 md:w-1/2 top-1/2 left-0 transform -translate-y-1/2 ml-5 md:ml-20">
-                    <p class="text-white text-base md:text-4xl lg:text-6xl">Comprometidos a promover la transparencia y la <span class="font-bold">publicidad de los intereses de su organización</span></p>
+                    <p class="text-white text-base md:text-4xl lg:text-6xl">
+                        Comprometidos a promover la transparencia y la 
+                        <span class="font-bold">publicidad de su organización para generar mayor confiabilidad.</span></p>
                     <div class="mt-3 md:mt-10 flex flex-col md:flex-row md:items-center">
                         <div>
                             <a class="btn btn-primary" href="{{ route('about') }}">ACERCA DE NOSOTROS</a>
