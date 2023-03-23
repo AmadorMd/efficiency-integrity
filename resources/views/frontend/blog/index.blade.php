38b2d71d1024d5eb5db8b57ed5d304348a5e6c33
Luis Amador <amadormd912@gmail.com>
2023-03-22 19:11:14 -0600

    fix: replace bg-header blogs and service section

--- resources/views/frontend/blog/index.blade.php
+++ resources/views/frontend/blog/index.blade.php
@@ -8,7 +8,7 @@
     <div class="px-6">
         <div class="text-center px-0 md:px-40 mb-10">
             <p class="text-lg md:text-xl text-primary">
-                Somos formadores de una comunidad empresarial que promueva la integridad, ya que es fundamental para construir una sociedad justa y equitativa. Las políticas de integridad en las empresas son esenciales para fomentar la transparencia, la honestidad y la ética en el mundo de los negocios. ¡Es hora de que las empresas tomen medidas concretas para mantenerse en línea con sus valores y actuar con integridad en todas las áreas de su actividad
+                Somos formadores de una comunidad empresarial que promueva la integridad, ya que es fundamental para construir una sociedad justa y equitativa. Las políticas de integridad en las empresas son esenciales para fomentar la transparencia, la honestidad y la ética en el mundo de los negocios. ¡Es hora de que las empresas tomen medidas concretas para mantenerse en línea con sus valores y actuar con integridad en todas las áreas de su actividad!
             </p>
         </div>
         <div class="text-center mb-5">
